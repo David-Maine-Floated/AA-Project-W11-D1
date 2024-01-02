00a55c653d614c6ea816797866c222bd427eaa0e
Yaqi Fang <cfang322@gmail.com>
2024-01-02 14:24:20 -0500

    finish phase 5

--- src/components/Hygrometer/Hygrometer.jsx
+++ src/components/Hygrometer/Hygrometer.jsx
@@ -1,14 +1,17 @@
 import ReactSlider from 'react-slider';
 import './Hygrometer.css';
+import { useState } from 'react';
 
 function Hygrometer() {
+  let [humidity, setHumidity] = useState(40);
+
   return (
     <section>
       <h2>Hygrometer</h2>
       <div className="actual-humid">Actual Humidity: {"y"}%</div>
       <ReactSlider
-        value={10}
-        onAfterChange={(val) => {}}
+        value={humidity}
+        onAfterChange={(val) => {setHumidity(val)}}
         className="hygrometer-slider"
         thumbClassName="hygrometer-thumb"
         trackClassName="hygrometer-track"
