b2720fe8c372fe882752dc5afa9220257a307765
Yaqi Fang <cfang322@gmail.com>
2024-01-02 14:00:13 -0500

    finish phase 3

--- src/main.jsx
+++ src/main.jsx
@@ -3,11 +3,14 @@ import ReactDOM from 'react-dom/client';
 import App from './App';
 import './index.css';
 import ThemeProvider from './context/ThemeContext';
+import ClimateThemeProvider from './context/ClimateContext';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
+    <ClimateThemeProvider>
       <ThemeProvider>
         <App />
       </ThemeProvider>
+    </ClimateThemeProvider>
   </React.StrictMode>
 );
